b4d7cffddee1c8f9aefed11db5707f33a7268d2a
mike-whypred-mm <michael@whypred.com>
2025-06-23 02:41:05 +1000

    fix sidebar second time

--- app.py
+++ app.py
@@ -23,7 +23,14 @@ hide_streamlit_style = """
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 header {visibility: hidden;}
-
+[data-testid="stSidebar"][aria-expanded="true"] {
+    min-width: 400px;
+    max-width: 400px;
+}
+[data-testid="stSidebar"][aria-expanded="false"] {
+    min-width: 0px;
+    max-width: 0px;
+}
 </style>
 """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
